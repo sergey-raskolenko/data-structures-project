e7d22d377008bc3d56f3f8d0f8f7c8cd9dc33199
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-06-11 18:16:19 +0300

    Added classes Node, Stak with method push

--- src/stack.py
+++ src/stack.py
@@ -1,29 +1,34 @@
 class Node:
     """Класс для узла стека"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
+        :param next_node: следующие данные узла
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
     """Класс для стека"""
 
     def __init__(self):
-        """Конструктор класса Stack"""
-        pass
+        """Инициализация пустого стэка класса Stack"""
+        self.top = None
 
-    def push(self, data):
+
+    def push(self, data_to_stack: Node):
         """
         Метод для добавления элемента на вершину стека
 
-        :param data: данные, которые будут добавлены на вершину стека
+        :param data_to_stack: данные, которые будут добавлены на вершину стека
         """
-        pass
+        node_in_stack = Node(data= data_to_stack)
+        node_in_stack.next_node = self.top
+        self.top = node_in_stack
 
     def pop(self):
         """
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,32 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+from src.stack import Node, Stack
+import unittest
+
+class TestNode(unittest.TestCase):
+
+	def test__init__(self):
+		n1 = Node(5, None)
+		n2 = Node('a', n1)
+		self.assertIsInstance(n1, Node)
+		self.assertIsInstance(n2, Node)
+		self.assertEqual(n1.data, 5)
+		self.assertEqual(n2.data, 'a')
+		self.assertEqual(n1, n2.next_node)
+
+class TestStack(unittest.TestCase):
+
+	def test_stack_from_main(self):
+		stack = Stack()
+		stack.push('data1')
+		stack.push('data2')
+		stack.push('data3')
+		self.assertEqual(stack.top.data, 'data3')
+		self.assertEqual(stack.top.next_node.data, 'data2')
+		self.assertEqual(stack.top.next_node.next_node.data, 'data1')
+		self.assertEqual(stack.top.next_node.next_node.next_node, None)
+		with self.assertRaises(AttributeError):
+			print(stack.top.next_node.next_node.next_node.data)
+
+
+if __name__ == '__main__':
+	unittest.main()
