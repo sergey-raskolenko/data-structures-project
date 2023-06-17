0e876c269bc69533ab030afe2ccd749a78950f3c
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-06-17 20:51:01 +0300

    Changes in stack

--- src/stack.py
+++ src/stack.py
@@ -1,12 +1,9 @@
 from src.node import Node
 class Stack:
     """Класс для стека"""
-
     def __init__(self):
         """Инициализация пустого стэка класса Stack"""
         self.top = None
-
-
     def __str__(self) -> str:
         """
         Отображает содержимое стэка от вершины
@@ -17,21 +14,17 @@ class Stack:
             stack_output.append(str(node_in_stack.data))
             node_in_stack = node_in_stack.next_node
         return '\n'.join(stack_output)
-
     def push(self, data_to_stack: Node):
         """
         Метод для добавления элемента на вершину стека
-
         :param data_to_stack: данные, которые будут добавлены на вершину стека
         """
         node_in_stack = Node(data= data_to_stack)
         node_in_stack.next_node = self.top
         self.top = node_in_stack
-
     def pop(self):
         """
         Метод для удаления элемента с вершины стека и его возвращения
-
         :return: данные удаленного элемента
         """
         data_to_return = self.top
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,10 +1,16 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
-from src.stack import Stack
-from src.node import Node
 import unittest
-
+from src.stack import Stack
 class TestStack(unittest.TestCase):
-
+	def test__init__(self):
+		stack = Stack()
+		self.assertEqual(stack.top, None)
+	def test__str__(self):
+		stack = Stack()
+		stack.push(True)
+		stack.push('data2')
+		stack.push(3)
+		self.assertEqual(stack.__str__(), '3\ndata2\nTrue')
 	def test_push_to_stack(self):
 		stack = Stack()
 		stack.push('data1')
@@ -16,28 +22,19 @@ class TestStack(unittest.TestCase):
 		self.assertEqual(stack.top.next_node.next_node.next_node, None)
 		with self.assertRaises(AttributeError):
 			print(stack.top.next_node.next_node.next_node.data)
-
 	def test_pop_from_stuck_1(self):
 		stack = Stack()
 		stack.push('data1')
 		data = stack.pop()
 		self.assertEqual(stack.top, None)
 		self.assertEqual(data, 'data1')
-
 	def test_pop_from_stuck_2(self):
 		stack = Stack()
 		stack.push('data1')
 		stack.push('data2')
 		data = stack.pop()
-		self.assertEqual(stack.top.data, 'data1')
 		self.assertEqual(data, 'data2')
-
-	def test_str_stuck(self):
-		stack = Stack()
-		stack.push(True)
-		stack.push('data2')
-		stack.push(3)
-		self.assertEqual(str(stack), '3\ndata2\nTrue')
+		self.assertEqual(stack.top.data, 'data1')
 
 
 if __name__ == '__main__':
